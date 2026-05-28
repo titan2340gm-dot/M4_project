2e8fe2b91458928f6b3386720b4f8d7868ad2015
Роман Костюков <romak7391@gmail.com>
2026-05-28 09:34:11 +0300

    изменена функция utils.py

---
+++ utils.py
@@ -0,0 +1,19 @@
+def human_turn(sticks_left):
+    while True:
+        try:
+            take=int(input(f"Ваш ход. Осталось {sticks_left} спичек. Сколько возьмете (1-3)?:"
+            if 1<=take<=min(3,sticks_left):
+                return take
+            else:
+                print("Ошибка, Можно взять от 1 до 3 спичек")
+        except ValueError:
+            print("Ошибка. Введите целое число")
+
+def computer_turn(sticks_left):
+    remainder = sticks_left % 4
+    if remainder != 0:
+        comp_take = remainder
+    else:
+        comp_take = random.randint(1, min(3, sticks_left))
+    print(f"Компьютер берет {comp_take} спичек")
+    return comp_take
