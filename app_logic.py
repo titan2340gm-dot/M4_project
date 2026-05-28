2ccd20d7f4d2ab7dd34613fe7ddd793ec77ee7b4
Роман Костюков <romak7391@gmail.com>
2026-05-28 09:39:46 +0300

    Добавлена функция check_winner

--- app_logic.py
+++ app_logic.py
@@ -11,3 +11,7 @@ def play_game(human_first, sticks):
             winner= "Вы победили!" if current_player else: "Победил компьютер"
             print(winner)
             break
+def check_winner(sticks_left):
+    if sticks_left==0:
+        return True
+    return False
